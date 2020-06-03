c2c020921b741fe00b2cef212cc0eb5c4a03e415
tanviredu <ornobtanvir.git@gmail.com>
2020-06-03 19:09:25 +0600

    added

---
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/dist/first.js
@@ -0,0 +1,11 @@
+var First = (function () {
+    function First() {
+        this.age = 10;
+        this.name = "tanvir Rahman";
+    }
+    return First;
+}());
+var f = new First();
+console.log(f.name);
+console.log(f.age);
+//# sourceMappingURL=first.js.map
---
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/first.ts
@@ -0,0 +1,11 @@
+
+class First {
+    age:number = 10;
+    name:string = "tanvir Rahman";
+    // this is a comment
+
+}
+
+var f = new First();
+console.log(f.name);
+console.log(f.age);
