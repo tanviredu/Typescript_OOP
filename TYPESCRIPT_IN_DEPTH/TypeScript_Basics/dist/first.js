b4d1d57d9aff5d37b71930093dbe90c44749d2ab
tanviredu <ornobtanvir.git@gmail.com>
2020-06-03 22:15:25 +0600

    added

--- TYPESCRIPT_IN_DEPTH/TypeScript_Basics/dist/first.js
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/dist/first.js
@@ -1,11 +1,60 @@
-var First = (function () {
-    function First() {
-        this.age = 10;
-        this.name = "tanvir Rahman";
+var Category;
+(function (Category) {
+    Category[Category["BioGraphy"] = 0] = "BioGraphy";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+var LibraryManager = (function () {
+    function LibraryManager() {
     }
-    return First;
+    LibraryManager.prototype.getAllBook = function () {
+        this.books = [
+            { title: "annodin", author: "humayon ahmead", avilable: true, category: Category.Fiction },
+            { title: "A Frawell", author: "Ernest", avilable: false, category: Category.Fiction },
+            { title: "I know the Cage bird sings", author: "Maya", avilable: true, category: Category.Poetry },
+            { title: "its 3 pm lets kill some people", author: "Aaaron", avilable: true, category: Category.History },
+        ];
+        return this.books;
+    };
+    LibraryManager.prototype.LogFirstAvilable = function (book) {
+        this.numberofBooks = book.length;
+        this.firstAvilable = "";
+        for (var _i = 0, book_1 = book; _i < book_1.length; _i++) {
+            var currentbook = book_1[_i];
+            if (currentbook.avilable) {
+                this.firstAvilable = currentbook.title;
+                break;
+            }
+        }
+        console.log(this.firstAvilable);
+    };
+    LibraryManager.prototype.getBookCategoryByTitle = function (categoryFilter) {
+        console.log("Getting Books in Category " + Category[categoryFilter]);
+        var allBooks = this.getAllBook();
+        var filteredTitles = [];
+        for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
+            var currentbook = allBooks_1[_i];
+            if (currentbook.category === categoryFilter) {
+                filteredTitles.push(currentbook.title);
+            }
+        }
+        return filteredTitles;
+    };
+    LibraryManager.prototype.LogBookTitles = function (titles) {
+        for (var _i = 0, titles_1 = titles; _i < titles_1.length; _i++) {
+            var title = titles_1[_i];
+            console.log(title);
+        }
+    };
+    return LibraryManager;
 }());
-var f = new First();
-console.log(f.name);
-console.log(f.age);
+var lb = new LibraryManager();
+var books = lb.getAllBook();
+lb.LogFirstAvilable(books);
+var num = Category.Fiction;
+var alltitle = lb.getBookCategoryByTitle(num);
+console.log(alltitle);
+lb.LogBookTitles(alltitle);
 //# sourceMappingURL=first.js.map
--- TYPESCRIPT_IN_DEPTH/TypeScript_Basics/first.ts
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/first.ts
@@ -1,11 +1,85 @@
 
-class First {
-    age:number = 10;
-    name:string = "tanvir Rahman";
-    // this is a comment
+enum Category{
+    // it will assign number
+    // to each category
+   BioGraphy,
+   Poetry,
+   Fiction,
+   History,
+   Children
 
 }
 
-var f = new First();
-console.log(f.name);
-console.log(f.age);
+class LibraryManager{
+
+     books:any
+     numberofBooks:number;
+     firstAvilable:string;
+
+
+    getAllBook():any{
+         this.books = [
+            {title:"annodin",author:"humayon ahmead",avilable:true,category:Category.Fiction},
+            {title:"A Frawell",author:"Ernest",avilable:false,category:Category.Fiction},
+            {title:"I know the Cage bird sings",author:"Maya",avilable:true,category:Category.Poetry},
+            {title:"its 3 pm lets kill some people",author:"Aaaron",avilable:true,category:Category.History},
+
+        ];
+
+        return this.books;
+    }
+
+    LogFirstAvilable(book:any):void{
+        this.numberofBooks = book.length;
+        this.firstAvilable = "";
+        // you have to use of
+        // insed of in
+
+        for (var currentbook of book){
+            //console.log(currentbook.title);
+            // run some checks
+            if(currentbook.avilable){
+                this.firstAvilable = currentbook.title;
+                break;
+            }
+        }
+
+        console.log(this.firstAvilable);
+
+    }
+
+    getBookCategoryByTitle(categoryFilter:Category):Array<string>{
+        console.log("Getting Books in Category "+Category[categoryFilter]);
+        const allBooks = this.getAllBook();
+        const filteredTitles:string[] = [];
+
+        for(var currentbook of allBooks){
+            if(currentbook.category === categoryFilter){
+                filteredTitles.push(currentbook.title);
+            }
+        }
+        return filteredTitles;
+
+    }
+
+    LogBookTitles(titles:string[]):void{
+        for(let title of titles){
+            console.log(title);
+        }
+    }
+
+
+    }
+
+
+
+var lb:LibraryManager = new LibraryManager();
+var books:any = lb.getAllBook();
+lb.LogFirstAvilable(books);
+var num:Category =Category.Fiction;
+var alltitle:string[] = lb.getBookCategoryByTitle(num);
+console.log(alltitle);
+lb.LogBookTitles(alltitle);
+
+
+
