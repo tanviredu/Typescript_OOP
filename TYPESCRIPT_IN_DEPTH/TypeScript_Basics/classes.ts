b4ff4e24a84fa35396e9f35a25b4b58f883e3ba0
tanviredu <ornobtanvir.git@gmail.com>
2020-06-04 13:02:19 +0600

    added

--- TYPESCRIPT_IN_DEPTH/TypeScript_Basics/classes.ts
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/classes.ts
@@ -16,3 +16,62 @@ class UniversityLibrarian implements Librarian{
     name: string;
 
 }
+
+/*
+ YOU CAN INSTANTIATE A ABSTRACT CLASS
+ YOU CAN ONLY INSTANTITE THE DERIVED CLASS
+ UNLIKE THE INTERFACE YOU CAN WRITE
+ SOME IMPLMEMTATION IN IT
+ */
+
+abstract class ReferenceItem{
+    // this abstract class
+    // will hep to find the information about
+    // from from given book and year
+
+    /*
+       ABSTRATC IS LIKE INTERFFACE
+       BUT YOU CAN PROVIDE SOME INFORMATION TOO
+     */
+
+    private _publisher:string;
+    static department:string = "Research";
+
+    constructor(public title:string,public year:number) {
+        console.log("Creating a new Refrance ...");
+
+    }
+    printItem():void{
+        console.log(this.title+ " has published "+this.year);
+
+        // this is static
+        console.log(" The department is "+ReferenceItem.department);
+    }
+
+    get publisher():string{
+        return this._publisher.toUpperCase();
+    }
+
+    set publisher(newPublisher:string){
+        this._publisher = newPublisher;
+    }
+
+    abstract printCitation():void;
+}
+
+
+class Encyclopedia extends ReferenceItem{
+    constructor(newTitle:string,newYear:number,public edition:number) {
+        super(newTitle,newYear);
+    }
+
+    printItem() {
+        super.printItem();
+        console.log("Edition" + this.edition +" "+ this.year);
+    }
+
+    printCitation(): void {
+        console.log("print citation");
+    }
+}
+
--- TYPESCRIPT_IN_DEPTH/TypeScript_Basics/dist/classes.js
+++ TYPESCRIPT_IN_DEPTH/TypeScript_Basics/dist/classes.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var UniversityLibrarian = (function () {
     function UniversityLibrarian() {
     }
@@ -6,4 +19,43 @@ var UniversityLibrarian = (function () {
     };
     return UniversityLibrarian;
 }());
+var ReferenceItem = (function () {
+    function ReferenceItem(title, year) {
+        this.title = title;
+        this.year = year;
+        console.log("Creating a new Refrance ...");
+    }
+    ReferenceItem.prototype.printItem = function () {
+        console.log(this.title + " has published " + this.year);
+        console.log(" The department is " + ReferenceItem.department);
+    };
+    Object.defineProperty(ReferenceItem.prototype, "publisher", {
+        get: function () {
+            return this._publisher.toUpperCase();
+        },
+        set: function (newPublisher) {
+            this._publisher = newPublisher;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    ReferenceItem.department = "Research";
+    return ReferenceItem;
+}());
+var Encyclopedia = (function (_super) {
+    __extends(Encyclopedia, _super);
+    function Encyclopedia(newTitle, newYear, edition) {
+        var _this = _super.call(this, newTitle, newYear) || this;
+        _this.edition = edition;
+        return _this;
+    }
+    Encyclopedia.prototype.printItem = function () {
+        _super.prototype.printItem.call(this);
+        console.log("Edition" + this.edition + " " + this.year);
+    };
+    Encyclopedia.prototype.printCitation = function () {
+        console.log("print citation");
+    };
+    return Encyclopedia;
+}(ReferenceItem));
 //# sourceMappingURL=classes.js.map
